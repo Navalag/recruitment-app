ec6cbe8329039fe5870d7508376fb2196fbbcc83
Navalag <agalavan@student.unit.ua>
2019-06-04 12:27:54 +0300

    initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('customer.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+
+        $requestData = $request->all();
+
+        Customer::create($requestData);
+
+        Session::flash('flash_message', 'Customer added!');
+
+        return redirect('customer');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $customer = Customer::findOrFail($id);
+
+        return view('customer.show', compact('customer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $customer = Customer::findOrFail($id);
+
+        return view('customer.edit', compact('customer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, Request $request)
+    {
+
+        $requestData = $request->all();
+
+        $customer = Customer::findOrFail($id);
+        $customer->update($requestData);
+
+        Session::flash('flash_message', 'Customer updated!');
+
+        return redirect('customer');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Customer::destroy($id);
+
+        Session::flash('flash_message', 'Customer deleted!');
+
+        return redirect('customer');
+    }
+}
